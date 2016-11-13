4cf97d4ce2e336d53b3853da3ca0ba7ddaeff9c8
Edy <support@mrreacher.com>
2016-11-13 17:26:29 +0200

    Create index.php

---
+++ CheatsTW/index.php
@@ -0,0 +1,397 @@
+<!DOCTYPE html>
+<?php
+if (stripos($_SERVER['HTTP_USER_AGENT'], "mobile")>0 or stripos($_SERVER['HTTP_USER_AGENT'], "Mobile")>0) {
+echo '<html>';
+} else {
+echo '<html style="overflow: hidden;">';
+}
+?>
+<head>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-7969276241011811",
+    enable_page_level_ads: true
+  });
+</script>
+<meta charset="UTF-8">
+<meta name="description" content="Cel mai nedetectabil cod pentru SA:MP aparut pe piata! 100% Nedetectabil! Intra sa vezi ce functii are!">
+<meta name="keywords" content="renko,aim samp,banu ramane,banul ramane,bugged.ro,tibi si shad0w,good bombardieru,aim,wall samp,servere samp,cheatstw.com,cheatstw,key-uri cheatstw,coduri samp,awaketuber,admini prafi,iblast mucosu care roade osu">
+<meta name="author" content="Autorul ramane anonim">
+<link rel="icon" type="image/png" href="favicon.png" sizes="128x128">
+
+<style>
+.pace {
+  -webkit-pointer-events: none;
+  pointer-events: none;
+
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+}
+
+.pace-inactive {
+  display: none;
+}
+
+.pace .pace-progress {
+  background: chartreuse;
+  position: fixed;
+  z-index: 2000;
+  top: 0;
+  right: 100%;
+  width: 100%;
+  height: 2px;
+}
+
+</style>
+
+<style>
+a, a:active, a:focus{
+outline: none; 
+}
+.btn:focus,.btn:active {
+outline: none !important;
+}
+</style>
+<script src="https://raw.githubusercontent.com/HubSpot/pace/v1.0.0/pace.min.js"></script>
+<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+ <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script src="/scroll.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<title>CheatsTW</title>
+
+</head>
+<body style="font-size: 16px; text-align: center; font-family: 'Montserrat', sans-serif; cursor: default; -moz-user-select: none; -webkit-user-select: none; -ms-user-select:none; user-select:none;-o-user-select:none; color: chartreuse; background: url(
+<?php
+$rand = rand(0,5);
+if($rand == 0) {
+	echo "https://i.imgur.com/5MxhxOO.jpg";
+} elseif($rand == 1) {
+	echo "https://i.imgur.com/bU3qJ90.jpg";
+} elseif($rand == 2) {
+	echo "https://i.imgur.com/RHKLGx6.jpg";
+} elseif($rand == 3) {
+	echo "https://i.imgur.com/2hiubG6.jpg";
+} elseif($rand == 4) {
+	echo "https://i.imgur.com/FQzueb8.jpg";
+} elseif($rand == 5) {
+	echo "https://i.imgur.com/P21KnkT.jpg";
+} elseif($rand == 6) {
+	echo "https://i.imgur.com/NdBlw4j.jpg";
+} else {
+	// NIMIC
+}
+?>) no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;">
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/56c20b278d4a470c0cfeb613/1ambc17pj';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+<script>
+$(document).ready(function(){
+<?php
+if (stripos($_SERVER['HTTP_USER_AGENT'], "firefox")>0) {
+echo "$('#fade1').fadeIn('slow');";
+echo "$('#fade2').fadeIn(4000);";
+echo "$('#status').fadeIn(6000);";
+} else {
+echo "$('#fade1').show('slow');";
+echo "$('#fade2').show(4000);";
+echo "$('#status').show(6000);";
+}
+?>
+$('[data-toggle="tooltip"]').tooltip();
+});
+</script>
+
+<script>
+$(document).ready(
+function() {
+$("html").niceScroll();
+}
+);
+</script>
+
+<div id="fade1" style="display:none; position: fixed; top: 0px; width: 100%; height: 100%; background-color: black; filter: alpha(opacity=60); opacity: 0.6;">
+</div>
+
+<div id="fade2" style="display:none; position: relative; top: 0px; width: 100%; height: 100%;">
+<div class="container">
+
+<style>
+.owner {
+    -webkit-animation-duration: 1s;
+    -moz-animation-duration: 1s;
+    -o-animation-duration: 1s;
+    animation-duration: 1s;
+    -webkit-animation-iteration-count: infinite;
+    -moz-animation-iteration-count: infinite;
+    -o-animation-iteration-count: infinite;
+    animation-iteration-count: infinite;
+    -webkit-animation-name: bounce_owner;
+    -moz-animation-name: bounce_owner;
+    -o-animation-name: bounce_owner;
+    animation-name: bounce_owner;
+    color: crimson;
+    font-family: Museo;
+    font-weight: bold;
+    line-height: 1em;
+    text-shadow: 0 0 10px purple;
+}
+
+@-moz-keyframes bounce_owner {
+0% {
+    text-shadow: 0 0 12px crimson, 0 0 16px crimson;
+}
+50% {
+    background: url("https://files.enjin.com/39829/images/particle.gif") repeat scroll 0 0 rgba(0, 0, 0, 0);
+    text-shadow: 0 0 12px purple, 0 0 16px purple;
+}
+100% {
+    text-shadow: 0 0 12px #C300C9, 0 0 16px #C300C9;
+}
+
+@-webkit-keyframes bounce_owner {
+0% {
+    text-shadow: 0 0 12px #FF15BE, 0 0 16px #FF15BE;
+}
+50% {
+    background: url("https://files.enjin.com/39829/images/particle.gif") repeat scroll 0 0 rgba(0, 0, 0, 0);
+    text-shadow: 0 0 12px #FFFFFF, 0 0 16px #FFFFFF;
+}
+100% {
+    text-shadow: 0 0 12px #C300C9, 0 0 16px #C300C9;
+}
+
+@-o-keyframes bounce_owner {
+0% {
+    text-shadow: 0 0 12px #FF15BE, 0 0 16px #FF15BE;
+}
+50% {
+    background: url("https://files.enjin.com/39829/images/particle.gif") repeat scroll 0 0 rgba(0, 0, 0, 0);
+    text-shadow: 0 0 12px #FFFFFF, 0 0 16px #FFFFFF;
+}
+100% {
+    text-shadow: 0 0 12px #C300C9, 0 0 16px #C300C9;
+}
+</style>
+
+<h1 class="owner" style="font-family: 'Amatic SC', cursive; font-size: 120px; color: orangered;">CheatsTW</h1>
+<br>
+<p>Salut, cauti un cod bun pentru SA:MP nedetectabil?</p>
+<p>Vrei sa nu fi banat pentru asa ceva?</p>
+<br>
+
+<!--START-->
+
+<?php
+If(isset($_POST['key'])) {
+$key = $_POST['key'];
+
+$servername = 'sub.domain.com';
+$username = 'user_name';
+$password = 'pass_word123';
+$dbname = 'user_name';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$keya = mysqli_real_escape_string($conn, $key);
+$query = mysqli_query($conn, "SELECT cd_key FROM cheatstw WHERE cd_key='$keya'"); // if key exists
+$setuser = mysqli_query($conn, "SELECT user,ip FROM cheatstw WHERE cd_key='$keya'"); // get username
+$download = mysqli_query($conn, "SELECT download FROM cheatstw WHERE cd_key='$keya'"); // check if downloaded or not
+$checkdownload = $download->fetch_assoc();
+$updatedownload = mysqli_query($conn, "UPDATE cheatstw SET download=1 WHERE cd_key='$keya'");
+$getuser = $setuser->fetch_assoc();
+$ip = $_SERVER['REMOTE_ADDR'];
+
+if($keya == null or preg_match("/'/",$keya) or preg_match('/"/',$keya) or ctype_space($keya) or strlen($keya) > 16 or strlen($keya) < 0) {
+echo '<p style="color: crimson;font-size:40px;">Key-ul tau este invalid!</p>';
+} elseif(mysqli_num_rows($query) > 0) {
+if($checkdownload["download"] == 0){
+echo '<p style="font-size:40px;">Key valid!</p>';
+echo '<p style="font-size:20px;"><span style="color:crimson;">'.$getuser['user'].'</span>, multumim ca ai ales CheatsTW.</p><br>';
+echo '<button onclick="download()" class="btn btn-sm btn-success"><i class="glyphicon glyphicon-download-alt"></i> Download</button>';
+echo '<script>';
+echo 'function download() {
+	window.open("ILSpy_Master_2.2.0.1337_Binaries.rar");
+	window.location.replace("https://cheatstw.com");
+	}';
+echo '</script>';
+echo '<br></br>';
+if($conn->query($updatedownload) === TRUE){
+	// DACA VALOAREA LA DOWNLOAD A FOST 0 O FACE 1 la urmatoarea introducere a key-ului
+}
+} elseif($getuser['ip'] == $ip) {
+	echo '<p style="font-size:40px;">Key valid!</p>';
+	echo '<p style="font-size:20px;"><span style="color:crimson;">'.$getuser['user'].'</span>, din pacate ai descarcat arhiva deja. Asteapta pana la urmatorul update!';
+	echo '<br></br>';
+} else {
+	echo '<p style="font-size:20px;">Ce ai facut adminule? Ai incercat sa iei arhiva?</p>';
+	//CREAZA LOG
+	$output = fopen("bruteforcedownloadbyadminsamoarabibi.txt","a+"); // OPEN FILE
+	date_default_timezone_set("Europe/Bucharest"); // ROMANIA GMT
+	$sendout = "IP-ul ".$ip." a incercat sa descarce arhiva pe numele lui ".$getuser['user'].". | Data: ".date("d.m.Y H:i:s A")." | Client IP real: ".$getuser['ip']."\n"; //Write in $output
+	fwrite($output,$sendout); // Write algorithm
+	//readfile("http://128.199.125.61/ddos/?ip=".$ip);
+}
+echo '<p><i style="color:darkgray;" class="glyphicon glyphicon-cog"></i> Dupa ce ai descarcat arhiva:</p><br>';
+echo '<p>Pentru instalarea codului pe CD / DVD te rugam sa ne contactezi pe livechat!</p>';
+echo '<p>In caz ca ai urme vechi de cod, te putem ajuta sa le stergi! Acest serviciu vine inclus alaturi de cod.</p>';
+echo '<p>Te rugam sa descarci ultima versiune de TeamViewer sau poti descarca urmatoarele alternative:</p>';
+echo '<div class="btn-group">';
+echo '<button class="btn btn-success btn-sm" onclick="tw1();">TeamViewer</button>';
+echo '<button class="btn btn-success btn-sm" onclick="tw2();">Supremo</button>';
+echo '<button class="btn btn-success btn-sm" onclick="tw3();">Ammy Admin</button>';
+echo '<button class="btn btn-success btn-sm" onclick="tw4();">Aero Admin</button>';
+echo '</div>';
+echo '<script>';
+echo 'function tw1() { window.open("tw.rar"); } function tw2() { window.open("supremo.rar"); } function tw3() { window.open("Ammy.rar"); } function tw4() { window.open("AeroAdmin.rar"); }';
+echo '</script>';
+echo '<br></br>';
+echo '<p>Incepand cu v0.6, veti avea nevoie doar de un CD/DVD! Pentru orice eventualitate, va rugam sa va luati 2 CD/DVD-uri.</p>';
+echo '<br>';
+} else {
+    echo '<p style="color: crimson;font-size:40px;">Key-ul tau este invalid!</p>';
+	echo '<script>';
+	echo "setTimeout(function () { window.location.href= '/'; },10000);";
+	echo '</script>';
+}
+
+$conn->close();
+} else {
+?>
+<form id="authkey" method="POST" autocomplete="off" action="/">
+<p>Introdu mai jos key-ul pentru a descarca codul.</p>
+<div align="center" class="form-group">
+<input style="width: 25%;text-align: center;" class="form-control input-sm" id="key" type="password" name="key" maxlength="16">
+</div>
+<input hidden id="trimite" type="submit">
+</form>
+
+<?php
+}
+?>
+
+<!--STOP-->
+<br>
+<div id="status"></div>
+<script>
+window.onload = loadNowPlaying()
+function loadNowPlaying(){
+  $("#status").load("status.php");
+}
+setInterval(function(){loadNowPlaying()}, 3000);
+</script>
+<br>
+<?php
+/*echo '<p style="font-size: 25px; color:crimson;">GIVEAWAY!</p>';*/ //TITLE
+/*echo '<a class="e-widget" href="https://gleam.io/zGIuL/cheatstw-key-giveaway" rel="nofollow">CheatsTW Key Giveaway!</a><script type="text/javascript" src="https://js.gleam.io/e.js" async="true"></script>';
+echo '<br>';*/ // Giveaway #1
+/*echo '<a class="e-widget" href="https://gleam.io/1bcvT/cheatstw-giveaway-2" rel="nofollow">CheatsTW Giveaway #2</a><script type="text/javascript" src="https://js.gleam.io/e.js" async="true"></script>';
+echo '<br>';*/ // Giveaway #2
+echo '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- CheatsTW.com -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7969276241011811"
+     data-ad-slot="6352423082"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>';
+echo '<br>';
+?>
+<p><i style="color:mediumseagreen;" class="glyphicon glyphicon-question-sign"></i> Intrebari generale:</p><br>
+<p>Q: Cum pot sa fac rost de key?</p>
+<p>A: Pentru a face rost de key, te rugam sa ne contactezi pe livechat. GooD sau Unknown - Creatorul codului o sa iti raspunda. Sau poti sa dai click mai jos:</p>
+<a href="skype:facebook:dani.eremia.3?add" class="btn btn-success btn-sm" role="button">Skype GooD</a><br></br>
+<p>Q: Prin ce modalitate de plata pot cumpara key-ul?</p>
+<p>A: Key-ul se poate cumpara doar prin PaySafeCard, PayPal, Nettler, Skrill momentan.</p><br>
+<p>Q: Mai am nevoie de ceva in afara de suma de bani pentru licenta(key) codului?</p>
+<p>A: Da, ai nevoie de un CD sau DVD gol bagat prima data in PC la cererea celui care iti face codul. Unii baga CD-ul sau DVD-ul si il formateaza.</p><br>
+<p>Q: Am codul vostru fara sa mai bag key in el. Primesc suport?</p>
+<p>A: Nu, deoarece toate codurile crackuite sunt detectabile la TW, deoarece nu au licenta. Cei care nu vor sa dea bani pe cod, dar vor sa foloseasca codul cracked, o vor face pe riscul lor si nu raspundem de pagubele produse de versiunile piratate.</p><br>
+<p>Q: Ce avantaje am daca am codul cu licenta?</p>
+<p>A: Fata de cei care au codul cracked, vei primi suport din partea developerilor, vei deveni membru cu drepturi in echipa CheatsTW si vei avea acces la toate update-urile.</p><br>
+<p>Q: Care este treaba cu codurile care imita CheatsTW?</p>
+<p>A: Alte coduri anonime, "imita" metoda de ascundere de pe CheatsTW, dar nu e mai sigura decat codul in sine(codul care imita metoda). Metoda de ascundere a codului de pe CheatsTW este mult mai complexa si mult mai stabila decat alte coduri cu propriile lor metode de ascundere.</p><br>
+<p>Q: Este adevarat faptul ca voi trebuie sa setati o anumita partitie ca sa ruleze codul? De exemplu: "R:\"</p>
+<p>A: Da, deoarece aimbot-ul ruleaza doar pe o partitie si numele aimbot-ului nu va fi niciodata executabil.</p><br>
+<p>Q: Am auzit ca codul vostru a primit multe banuri pe motiv 7 zile, este adevarat?</p>
+<p>A: In nici un caz, codul nostru nu a primit ban pentru 7 zile, respectiv 90 de zile.</p><br>
+<p>Q: Cum pot prinde codul de pe CheatsTW?</p>
+<p>A: Daca esti admin si vrei sa stii, ai cateva variante: Codatul are mereu partitia R:\(nu prea este o dovada clara, dar poate fi banat 7 zile); Key pe email sau intr-un notepad(aici clar 90); Verifici daca este logat pe panel. Pentru mai multe detalii de prindere a codului, puteti intra <a class="nolink" href="http://demascaregood.3x.ro/" target="_blank">aici</a>.</p><br>
+<p>Q: De cate ori a fost demascat codul?</p>
+<p>A: Praduitorii au avut foarte multe tentative ca sa demascheze codul, dar fara succes. Un exemplu este mentionat la intrebarea de mai sus.</p><br>
+<p>Q: Acest program este 100% sigur?</p>
+<p>A: Da, daca vrei sa te convingi, da click mai jos pentru detalii.</p>
+<button class="btn btn-success btn-sm" onclick="dovada1();">Cheats TW on YouTube</button>
+<p></p>
+
+<script>
+function dovada1() {
+    window.open("https://www.youtube.com/channel/UCeknNkwBJZhrHcPPEMHAYrg");
+}
+</script>
+
+<br>
+<p>Q: Ce fac daca am pierdut cheia de acces de la cod?</p>
+<p>A: Din pacate, daca ai pierdut cheia de acces, te rugam sa iti verifici email-ul solicitat la cumpararea codului. Daca ai pierdut accesul la email nu mai poti recupera codul.</p><br>
+<p>Q: Cheia mea de acces a fost blocata, de ce?</p>
+<p>A: Posibil ca o persoana necunoscuta sa se fi logat pe key-ul tau. In caz ca iti spune "Ai dat key-ul la altcineva", da click pe butonul de mai jos.</p>
+<button class="btn btn-success btn-sm" onclick="panel();">Acceseaza panel-ul</button>
+<p></p>
+
+<script>
+function panel() {
+    window.open("https://cheatstw.com/panel");
+}
+</script>
+
+<br>
+<p>Q: Ce este inclus in cod si care sunt functiile acestuia?</p>
+<p>A: F1 > No-Recoil; F2 > Semi No-Recoil; F3 > Aimbot; Mousefix; TriggerBot; WallHack; etc. Mai multe detalii pe canalul de YouTube. Versiunea actuala este v3. Daca aveti idei pentru cod, va asteptam pe livechat!</p><br>
+<br>
+<p style="color:crimson; font-size:11px;">* Pentru a proteja codul de admini, fiecare client va avea cate o cheie proprie, la care doar jucatorii vor avea acces. Cei care vor sa cumpere o cheie pentru a accesa codul, sunt rugati sa lase in livechat datele de contact!</p>
+<p style="color:crimson; font-size:11px;">* Cheia nu poate fi folosita de mai multi jucatori sau impartita intre mai multi jucatori. Daca vrei si tu o cheie, pur si simplu cumpara un PaySafeCard de la magazin si te asteptam pe livechat!</p>
+<p style="color:crimson; font-size:11px;">* Daca faci parte din staff si vrei sa cumperi un key, din pacate nu vei avea dreptul la asa ceva! In caz de incerci sa ne pacalesti ca nu faci parte din staff, cheia ta catre accesul codului va fi suspendata!</p>
+
+<h3>Site gazduit de <span><a href="https://my.blazingfast.io/?affid=1280" target="_blank"><img src="https://i.imgur.com/yno3bac.png" data-toggle="tooltip" data-placement="bottom" title="BlazingFast.io" alt="BlazingFast.io"></img></a></span></h3>
+<sub style="color:crimson;">All Rights Reserved &reg; cheatstw.com</sub>
+<br></br><br></br>
+</div>
+</div>
+</body>
+</html>
+<style>
+a.nolink, a.nolink:hover, a.nolink:focus {
+outline: none;
+text-decoration: none;
+color: crimson;
+}
+</style>
